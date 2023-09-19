c91c818e2449d3e4bae382d3f96c6829a75a75f4
marwateaer <145429513+marwateaer@users.noreply.github.com>
2023-09-19 11:41:08 +0300

    Add files via upload

---
+++ uygulama5/Form1.cs
@@ -0,0 +1,18 @@
+using System.Windows.Forms;
+
+namespace uygulama5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Hello World";
+        }
+    
+    }
+}
